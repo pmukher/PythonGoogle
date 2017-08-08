eb827aed9e5520ae50b0ed791d5609412918b262
Pratik Mukherjee <pmukher@us.ibm.com>
2017-08-07 21:46:59 -0400

    Added multi line string and \ demo

--- StandardSring.py
+++ StandardSring.py
@@ -4,3 +4,8 @@ print len(s)
 print s+' there'
 pi = 3.14
 print 'The value of pi is '+str(pi)
+raw = "this\t\n and that"
+print raw 
+multi = """This was the better of the times 
+It was the best of the times"""
+print multi 
