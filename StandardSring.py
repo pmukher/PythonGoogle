607690cf42a19ee7bcb8e14446ce107be7b23d26
Pratik Mukherjee <pmukher@us.ibm.com>
2017-08-07 22:01:14 -0400

    Added various other string functionalities

--- StandardSring.py
+++ StandardSring.py
@@ -9,3 +9,11 @@ print raw
 multi = """This was the better of the times 
 It was the best of the times"""
 print multi 
+print '-'.join(['aaa','bbb','ccc'])
+print "Hi there".find("the")
+print "Hi there".split(" ")
+print "Hi there".upper()
+print "   Hi there.   ".strip(" ")
+print "Hi there.".replace("Hi", "Hello")
+print "Hithere".isalpha()
+print "Hi there".startswith("Hi")
